9182f046930d7024e5e3506f73f3f6cfcc6d71c3
IsiKekee <kekekuza@gmail.com>
2023-04-30 22:59:34 +0300

    ignore vendor and .idea

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,10 @@
 <?php
+
+use Yoku\Ddd\Application\Services\EntityMangerService;
+
+require_once '../../vendor/autoload.php';
+
+$em = new EntityMangerService();
+
+$db = $em ->getEm();
+
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitcea3d528ebfa925fdd99164be61db74c
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcea3d528ebfa925fdd99164be61db74c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitcea3d528ebfa925fdd99164be61db74c', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitcea3d528ebfa925fdd99164be61db74c
         call_user_func(\Composer\Autoload\ComposerStaticInitcea3d528ebfa925fdd99164be61db74c::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitcea3d528ebfa925fdd99164be61db74c::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
