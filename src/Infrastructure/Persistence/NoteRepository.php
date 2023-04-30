8df95174aa9b558d7ac73347a3357264d99e01c3
IsiKekee <kekekuza@gmail.com>
2023-04-30 22:51:58 +0300

    Note repository, persist note

--- src/Infrastructure/Persistence/NoteRepository.php
+++ src/Infrastructure/Persistence/NoteRepository.php
@@ -2,7 +2,27 @@
 
 namespace Yoku\Ddd\Infrastructure\Persistence;
 
-class NoteRepository
-{
 
+use Yoku\Ddd\Domain\Entity\Note;
+use Yoku\Ddd\Domain\Repository\NoteRepositoryInterface;
+
+class NoteRepository implements NoteRepositoryInterface
+{
+    private $db; // database connection object
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function save(Note $note): void
+    {
+        $stmt = $this->db->prepare('INSERT INTO notes (id, title, content, created_at) VALUES (:id, :title, :content, :created_at)');
+        $stmt->execute([
+            ':id' => $note->getId(),
+            ':title' => $note->getTitle(),
+            ':content' => $note->getContent(),
+            ':created_at' => $note->getCreatedAt()->format('Y-m-d H:i:s'),
+        ]);
+    }
 }
