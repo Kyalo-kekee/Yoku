d1c4063f20c294bd2711894b2ca5247bbd5a423c
IsiKekee <kekekuza@gmail.com>
2023-04-30 22:51:15 +0300

    Application note services

--- src/Application/Services/NoteService.php
+++ src/Application/Services/NoteService.php
@@ -2,7 +2,26 @@
 
 namespace Yoku\Ddd\Application\Services;
 
+
+use Yoku\Ddd\Domain\Entity\Note;
+use Yoku\Ddd\Domain\Repository\NoteRepositoryInterface;
+
 class NoteService
 {
+    private $noteRepository;
 
+    public function __construct(NoteRepositoryInterface $noteRepository)
+    {
+        $this->noteRepository = $noteRepository;
+    }
+
+    public function createNote(string $title, string $content): void
+    {
+        $note = new Note();
+        $note->setTitle($title);
+        $note->setContent($content);
+        $note->setId(uniqid());
+
+        $this->noteRepository->save($note);
+    }
 }
