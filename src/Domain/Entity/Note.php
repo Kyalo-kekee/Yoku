147b64bc4cc1f008304b05a5c6f2d6b016c99ff3
IsiKekee <kekekuza@gmail.com>
2023-04-30 22:52:42 +0300

    Note entity

--- src/Domain/Entity/Note.php
+++ src/Domain/Entity/Note.php
@@ -2,7 +2,91 @@
 
 namespace Yoku\Ddd\Domain\Entity;
 
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "notes")]
 class Note
 {
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private $title;
+
+    #[ORM\Column(type: "text")]
+    private $content;
+
+    #[ORM\Column(type: "datetime")]
+    private DateTime $createdAt;
+
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
 
 }
